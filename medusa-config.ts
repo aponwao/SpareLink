7ca5333cb9cc4010e582f8187b498150b8243e3f
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2025-03-07 06:39:15 -0400

    First commit - Starting B2B Medusa Project for Backend - Adding container - Created fly.toml file for worker instance and another for server instance

---
+++ medusa-config.ts
@@ -0,0 +1,59 @@
+import { QUOTE_MODULE } from "./src/modules/quote";
+import { APPROVAL_MODULE } from "./src/modules/approval";
+import { COMPANY_MODULE } from "./src/modules/company";
+import { loadEnv, defineConfig, Modules } from "@medusajs/framework/utils";
+
+loadEnv(process.env.NODE_ENV!, process.cwd());
+
+const REDIS_MODULE = "cache-redis";
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+    workerMode:
+      (process.env.MEDUSA_WORKER_MODE as "shared" | "server" | "worker") ||
+      "shared",
+    redisUrl: process.env.REDIS_URL,
+  },
+  modules: {
+    [COMPANY_MODULE]: {
+      resolve: "./modules/company",
+    },
+    [QUOTE_MODULE]: {
+      resolve: "./modules/quote",
+    },
+    [APPROVAL_MODULE]: {
+      resolve: "./modules/approval",
+    },
+    [Modules.CACHE]: {
+      resolve: "@medusajs/medusa/cache-inmemory",
+    },
+    [Modules.WORKFLOW_ENGINE]: {
+      resolve: "@medusajs/medusa/workflow-engine-inmemory",
+    },
+    [Modules.EVENT_BUS]: {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    [REDIS_MODULE]: {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+  },
+  admin: {
+    disable: Boolean(process.env.DISABLE_MEDUSA_ADMIN) || false,
+    backendUrl: process.env.MEDUSA_BACKEND_URL ?? undefined,
+    path: "/dashboard",
+  },
+});
